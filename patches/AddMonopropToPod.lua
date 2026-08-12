8d9dc3ace55cfe7fab0d56cb583bd39f82868a53
Takovid <daytimera@gmail.com>
2026-08-12 21:46:33 +0300

    New Patch

--- patches/AddMonopropToPod.lua
+++ patches/AddMonopropToPod.lua
@@ -1,5 +1,7 @@
 PM.Parts:Patch("AddMonopropToPod")
-        :Named("pod_1v_command")
+        :Named("pod_1v_conical_crew")
         :Do(function(part)
-            part.resourceContainers:Add("MonoPropellant", 100, 100)
+            part.resourceContainers:PatchData("MonoPropellant", 1, 1)
         end)
+
+-- pod_1v_conical_crew
